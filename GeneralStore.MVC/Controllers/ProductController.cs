c20dff9a7f0cdd208817fe1f403dc899f61a63f3
Sabra Snider <sabralsnider@gmail.com>
2020-06-23 10:24:09 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+
+        // GET: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
